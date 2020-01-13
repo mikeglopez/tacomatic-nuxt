ee0400574edbe01d62807eccc09eecdc82b9fd90
Mike <michael.lopez@flowersfordreams.com>
2020-01-13 08:37:06 -0600

    Add express server

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-app>
+    <h1
+      class="heading text-center primary--text display-4 font-weight-black"
+    >
+      Tacomatic
+    </h1>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+  @import url('https://fonts.googleapis.com/css?family=Permanent+Marker&display=swap');
+  .heading {
+    font-family: 'Permanent Marker', cursive !important;
+  }
+</style>
