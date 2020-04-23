2474ba0dbab91aee65f4c2253c600c1b55ccc40b
Mike <mikeglopez.91@gmail.com>
2020-04-22 20:35:05 -0500

    Try share cross-origin resource

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,8 +47,17 @@ module.exports = {
    */
   modules: [
     // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios'
+    '@nuxtjs/axios',
+    '@nuxtjs/proxy'
   ],
+  proxy: {
+    '/api': {
+      target: 'http://localhost:3000/*',
+      pathRewrite: {
+        '^/api': '/'
+      }
+    }
+  },
   /*
    ** Axios module configuration
    ** See https://axios.nuxtjs.org/options
